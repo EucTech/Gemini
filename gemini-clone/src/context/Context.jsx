5e6cd8ed413389ab4085cccdb1bf012382894209
EucTech <euccreative@gmail.com>
2024-04-22 21:13:29 +0100

    updated

--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -9,16 +9,38 @@ const ContextProvider = (props) => {
   const [recentPrompt, setRecentPrompt] = useState("");
   const [prevPrompt, setPrevPrompt] = useState([]);
   const [showResult, setShowResult] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [resultData, setResultData] = useState("");
+
+  const delayPara = (index, nextword) => {
+    setTimeout(function () {
+      setResultData(prev=>prev+nextword);
+    }, 75*index)
+  }
 
   const onSent = async (prompt) => {
     setResultData("")
     setLoading(loading)
     setShowResult(true)
     setRecentPrompt(input)
+    setPrevPrompt(prev=>[...prev, input])
     const response = await runChat(input)
-    setResultData(response)
+    let responseArray = response.split("**");
+    let newResponse = "";
+    for(let i=0; i < responseArray.length; i++){
+      if ( i == 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      }
+      else {
+        newResponse += "<b>"+responseArray[i]+"</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+    let newResponseArray = newResponse2.split(" ");
+    for (let i=0; i<newResponseArray.length; i++) {
+      const nextword = newResponseArray[i];
+      delayPara(i, nextword+" ")
+    }
     setLoading(false)
     setInput("")
   }
